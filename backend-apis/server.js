62db260497428cab7435ec1d5654f61f10b5a738
Gonza-PG <gonzaloperezgrunau@hotmail.com.ar>
2024-12-06 12:09:17 -0300

    modulo mails

--- backend-apis/server.js
+++ backend-apis/server.js
@@ -5,15 +5,15 @@ const userRoutes = require('./routes/users');
 const projectRoutes = require('./routes/projects');
 const ticketRoutes = require('./routes/tickets');
 const gastoRoutes = require('./routes/gastos_miembros');
+const emailRoutes = require('./routes/mail'); // Importar las rutas de correo
 const swagger = require('swagger-ui-express');
 
 const app = express();
 const PORT = 8080;
 
-
 app.use(cors({ origin: 'http://localhost:3000' }));
 
-app.use(express.json({ limit: '10mb' }));  
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Usar las rutas
@@ -22,8 +22,10 @@ app.use('/api/projects', projectRoutes);
 app.use('/api/tickets', ticketRoutes);
 app.use('/api/gastos', gastoRoutes);
 
-app.use('/api-docs', swagger.serve, swagger.setup(require('./swagger2.json')));
+// Rutas para envío de correos
+app.use('/api', emailRoutes);  // Asegúrate de que las rutas de correos están bajo /api
 
+app.use('/api-docs', swagger.serve, swagger.setup(require('./swagger2.json')));
 
 // Sincronizar modelos con la base de datos
 db.sync()
--- backend-apis/services/mail.js
+++ backend-apis/services/mail.js
@@ -7,13 +7,27 @@ dotenv.config();
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async (email, subject, htmlTemplate) => {
-  resend.emails.send({
-    from: 'Ticketify <onboarding@resend.dev>',
-    to: email,
-    subject,
-    html: htmlTemplate
-  })
-}
+  try {
+    const response = await resend.emails.send({
+      from: 'Ticketify <onboarding@resend.dev>',
+      to: email,
+      subject,
+      html: htmlTemplate
+    });
+
+    if (response && response.data && response.data.id) {
+      console.log(`Correo enviado con éxito. ID: ${response.data.id}`);
+      return response.data.id; // Retorna el ID del correo si se envió correctamente
+    } else {
+      console.error('No se pudo enviar el correo, no se recibió un ID válido.');
+      throw new Error('No se pudo enviar el correo, no se recibió un ID válido.');
+    }
+  } catch (error) {
+    console.error('Error al enviar el correo:', error);
+    throw new Error('Error al enviar el correo: ' + error.message);
+  }
+};
+
 
 module.exports = {
   sendMail,
